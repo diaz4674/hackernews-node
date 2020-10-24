a49e9abe017e134ccbec7dcfaff82b9c3d2210e6
unknown <diaz4674@gmail.com>
2020-10-24 03:34:28 -0700

    added post/delete

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-    type Query {
-        info: String!
-    }
-`;
-
+let links = [
+	{
+		id: "link-0",
+		url: "www.howtographql.com",
+		description: "Fullstack tutorial for GraphQL",
+	},
+];
+let idCount = links.length;
 const resolvers = {
 	Query: {
 		info: () => `this is the api of a hacernews clone`,
+		feed: () => links,
+	},
+	Mutation: {
+		post: (parent, args) => {
+			const link = {
+				id: `link-${idCount++}`,
+				description: args.description,
+				url: args.url,
+			};
+			links.push(link);
+			return link;
+		},
+		delete: (parent, args) => {
+			const postIndex = links.findIndex((link) => {
+				if (link.id === args.id) {
+					return true;
+				}
+			});
+
+			if (postIndex === -1) {
+				throw new Error("link not found");
+			}
+
+			const deletedLink = links.splice(postIndex, 1);
+
+			return deletedLink[0];
+		},
 	},
 };
 
 const server = new GraphQLServer({
-	typeDefs,
+	typeDefs: "./src/schema.graphql",
 	resolvers,
 });
 
