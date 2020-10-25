168899d119ce2831429ea4dd903efca5519506ca
unknown <diaz4674@gmail.com>
2020-10-25 03:38:11 -0700

    added login/signup

--- src/index.js
+++ src/index.js
@@ -1,52 +1,27 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { PrismaClient } = require("@prisma/client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 
 const prisma = new PrismaClient();
 
 const resolvers = {
-	Query: {
-		info: () => `this is the api of a hacernews clone`,
-		feed: async (parent, args, context) => {
-			return context.prisma.link.findMany();
-		},
-	},
-	Mutation: {
-		post: (parent, args, context) => {
-			const newLink = context.prisma.link.create({
-				data: {
-					description: args.description,
-					url: args.url,
-				},
-			});
-
-			return newLink;
-		},
-
-		delete: (parent, args, context) => {
-			const deletedLink = context.prisma.link.delete({
-				where: { id: Number(args.id) },
-			});
-
-			return deletedLink;
-		},
-		update: (parent, args) => {
-			const { id, data } = args;
-
-			const link = prisma.link.update({
-				where: { id: Number(id) },
-				data,
-			});
-
-			return link;
-		},
-	},
+	Query,
+	Mutation,
+	User,
+	Link,
 };
 
 const server = new GraphQLServer({
 	typeDefs: "./src/schema.graphql",
 	resolvers,
-	context: {
-		prisma,
+	context: (request) => {
+		return {
+			...request,
+			prisma,
+		};
 	},
 });
 
